7f869da67f454f022f6b5e33aefe934dddd3fadb
Paul Caruana <paul.caruana@leighton.com>
2020-12-20 13:09:24 +0000

    completed annimations module

--- src/App.js
+++ src/App.js
@@ -36,7 +36,14 @@ class App extends Component {
               in={this.state.showBlock} 
               timeout={1000}
               mountOnEnter
-              unmountOnExit>
+              unmountOnExit
+              onEnter={() => console.log('onEnter')}
+              onEntering={() => console.log('onEntering')}
+              onEntered={() => console.log('onEntered')}
+              onExit={() => console.log('onExit')}
+              onExiting={() => console.log('onExiting')}
+              onExited={() => console.log('onExited')}
+            >
               {state => 
                 <div 
                   style={{
@@ -49,9 +56,14 @@ class App extends Component {
                   }}
                 />}
             </Transition>
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} /> : null}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen}/>: null}
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+
+                <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+        {this.state.modalIsOpen ? (
+          <Backdrop show/>
+        ): null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
